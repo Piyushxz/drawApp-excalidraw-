8a508ef421b3c5dd4ffc3c76bb015e0f3b58f401
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-01-12 22:55:46 +0530

    http route init

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hi")
+import express from "express";
+import { middleware } from "./middleware";
+
+const app = express()
+
+
+
+app.post("/signup",(req,res)=>{
+
+})
+app.post("/signin",(req,res)=>{
+    
+})
+
+app.post("/createroom",middleware,(req,res)=>{
+
+})
+
+
+
+app.listen(3008)
