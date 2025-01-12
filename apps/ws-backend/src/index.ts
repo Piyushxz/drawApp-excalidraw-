0a31c3bb30bead8615812a148bb387457479a5cd
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-01-12 23:10:26 +0530

    added ws logiv

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,1 +1,35 @@
-console.log("hi")
+import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken"
+
+const wss = new WebSocketServer({port:8080})
+
+
+
+wss.on("connection",(ws,request)=>{
+
+    const url = request.url
+
+
+    if(!url){
+        return;
+    }
+
+    const queryParams = new URLSearchParams(url.split('?')[1])
+    const token = queryParams.get('token') || ""
+
+
+    const decoded = jwt.decode(token,);
+    
+    if(!decoded || !(decoded as JwtPayload).userId){
+        ws.close();
+        return;
+    }
+
+    ws.on('message',(data)=>{
+        ws.send(data.toString())
+    })
+
+
+})
+
+
