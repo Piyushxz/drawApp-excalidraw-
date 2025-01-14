d5f6fcb86422902f4422d6de4fe6093e74791732
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-01-15 01:55:43 +0530

    all bug fixes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,32 +1,79 @@
 import express from "express";
 import { middleware } from "./middleware";
 import {signUpSchema,signin,createRoomSchema} from "@repo/common/types"
-import {z} from "zod"
+import {prismaClient} from "@repo/db/client"
+import {JWT_SECRET } from "@repo/backend-common/config"
+import jwt from "jsonwebtoken"
+import cors from "cors"
 const app = express()
 
 
 
-app.post("/signup",(req,res)=>{
-    const {username,password,email} = req.body;
+app.use(express.json())
+app.use(cors())
 
-    const data = signUpSchema.safeParse(req.body)
+
+
+app.post("/signup",async (req,res)=>{
+
+    const data =  signUpSchema.safeParse(req.body)
     if(!data.success){
         res.status(400).json({message:"Invalid format"})
         return;
     }
 
+    try{
+        await prismaClient.user.create({
+            data:{
+                username:data.data.username,
+                email:data.data.email,
+                password:data.data.password,
+
+            }
+        })
+
+        res.status(200).json({message:"User created successfully!"})
+    }catch(e){
+        res.status(500).json({message:"Could no signUp"})
+    }
+
 })
-app.post("/signin",(req,res)=>{
-        const {username,password} = req.body;
+app.post("/signin",async (req,res)=>{
 
     const data = signin.safeParse(req.body)
     if(!data.success){
         res.status(400).json({message:"Invalid format"})
         return;
     }
+
+    try{
+        let foundUser = null;
+
+        foundUser = await prismaClient.user.findFirst({where:
+            {
+                email:data.data.email,
+                password:data.data.password
+            }
+        })
+
+        if(!foundUser){
+            res.status(404).json({message:"Invalid Credentials"})
+            return;
+        }
+
+        const token = jwt.sign({id:foundUser.id},JWT_SECRET)
+
+        res.status(200).json({message:"Signed In successfully!",token:token})
+
+
+    }catch(e){
+        res.status(500).json({message:"Could no signUp"})
+
+    }
 })
 
 app.post("/createroom",middleware,(req,res)=>{
+    const userId = req.body.userId;
     
     const data = createRoomSchema.safeParse(req.body)
     if(!data.success){
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,28 @@
 import { NextFunction,Request,Response } from "express";
-
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+declare global {
+    namespace Express {
+      interface Request {
+        userId?: string;
+      }
+    }
+  }
+  
 export const middleware = (req:Request,res:Response,next:NextFunction)=>{
 
-    next()
+    const token = req.headers.token ;
+
+
+ 
+        
+        const decoded = jwt.verify(token as string, JWT_SECRET) as JwtPayload ;
+    
+        
+        req.userId = decoded.id;
+    
+        
+        next();
+      
+
 }
