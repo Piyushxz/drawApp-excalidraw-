b30e7429219bca30098b8d000ff6869815984580
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-03-10 19:20:42 +0530

    latest

--- apps/excalidraw-frontend/components/icons/Circle.tsx
+++ apps/excalidraw-frontend/components/icons/Circle.tsx
@@ -1,11 +1,10 @@
-import { CircleEllipsisIcon } from "lucide-react"
-import { iconStyles } from "."
+import {  CircleIcon } from "lucide-react"
 import { iconType } from "."
 export default function Circle(props:iconType){
     return(
         <div onClick={props.onClick}
         className={`p-3 rounded-lg ${props.selectedTool === "circle" ? " border border-gray-600 " :" hover:bg-gray-600/30"} `}>
-            <CircleEllipsisIcon className="text-white size-4"/>
+            <CircleIcon className="text-white size-4"/>
         </div>
     )
 }
