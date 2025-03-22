744ffb6677134cccfdca1220525c3255df7affd4
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-03-22 23:14:11 +0530

    improved sign up page

--- apps/excalidraw-frontend/app/(auth)/signup/page.tsx
+++ apps/excalidraw-frontend/app/(auth)/signup/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import LiquidChrome from "@/components/LiquidChrome";
+import Link from "next/link";
 export default function SignupPage() {
     return (
-        <div className="flex justify-center overflow-hidden min-h-screen bg-[#0f0f0f]">
-            <div className="overflow-hidden w-[80vw] h-[90vh] border border-white/30 rounded-lg mx-10 my-10 grid grid-cols-12">
+        <div className="flex justify-center  min-h-screen bg-[#0f0f0f]">
+            <div className=" md:w-[80vw] w-[90vw] max-h-auto border border-white/30 rounded-lg mx-2 md:mx-10 my-10 grid grid-cols-12">
                 {/* Left Section */}
                 <div className=" col-span-12 md:col-span-6 flex justify-center items-center p-6">
                     <div className="flex flex-col justify-center gap-6 w-90">
@@ -24,7 +25,7 @@ export default function SignupPage() {
 
                         {/* Heading */}
                         <div className="flex flex-col gap-1">
-                            <h1 className="font-semibold text-2xl text-white tracking-tight">Get Started</h1>
+
                             <h2 className="opacity-50 text-md text-white tracking-tight">
                                 Welcome to Slapdash – Let's create your account
                             </h2>
@@ -32,6 +33,14 @@ export default function SignupPage() {
                         <div className="my-2 w-90  border border-white/10"></div>
                         {/* Input Fields */}
                         <div className="flex flex-col gap-4">
+                        <div className="flex flex-col gap-1">
+                                <label className="text-white tracking-tighter text-sm">Email</label>
+                                <input
+                                    type="text"
+                                    className="w-full bg-[#191919] py-3 px-3 rounded-md text-white text-sm 
+                                    focus:outline-none focus:ring-2 focus:ring-[#65e6bf] transition-all"
+                                />
+                            </div>
                             <div className="flex flex-col gap-1">
                                 <label className="text-white tracking-tighter text-sm">Username</label>
                                 <input
@@ -59,23 +68,36 @@ export default function SignupPage() {
                         >
                             Sign up
                         </button>
+                        <div className="flex justify-center">
+                        <h1 className=" text-sm text-white/50 tracking-tighter ">Already Have an account ?<Link href="/signin" className="text-white opacity-95  "> Log in </Link></h1>
+
+                        </div>
+
                     </div>
                 </div>
 
                 {/* Right Section */}
-                <div className=" col-span-12 md:col-span-6 border border-white/30 flex items-center justify-center">
-                <div style={{ width: '100%', height: '100%', position: 'relative' }}>
-                <LiquidChrome
-                    baseColor={[0.16, 0.31, 0.29]}
-                    speed={0.1}
-                    amplitude={0.4}
-                    interactive={false}
-                />
+                <div className="col-span-12 md:col-span-6 flex items-center justify-center">
+                    <div 
+                        style={{ width: '100%', height: '95%', position: 'relative', borderRadius: "0.5rem" }} 
+                        className="mx-4 rounded-lg  mb-4 md:mb-1 "
+                    >
+                        <LiquidChrome
+                            baseColor={[0.16, 0.31, 0.29]}
+                            speed={0.1}
+                            amplitude={0.4}
+                            interactive={false}
+                        />
 
+                        {/* Positioned Text */}
+                        <div className="absolute inset-0 flex flex-col justify-center items-center backdrop-blur-md">
+                            <h1 className="md:text-4xl text-2xl font-semibold tracking-tighter text-white/80">Sketch.</h1>
+                            <h1 className="md:text-4xl text-2xl font-semibold tracking-tighter text-white/80">Collaborate.</h1>
+                            <h1 className="md:text-4xl text-2xl font-semibold tracking-tighter text-white/80">Create.</h1>
+                        </div>
+                    </div>
+</div>
 
-                
-                </div>
-                </div>
             </div>
         </div>
     );
