7e86cfb1eb3302c8c4eef6f5f7fd73fcd4a14dba
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-04-04 00:15:12 +0530

    added room modal

--- apps/excalidraw-frontend/components/CreateRoomModal.tsx
+++ apps/excalidraw-frontend/components/CreateRoomModal.tsx
@@ -1,33 +1,31 @@
-import { AnimatePresence, motion } from "motion/react";
-import { Dispatch,SetStateAction } from "react";
-
-export const CreateRoomModal = ({setIsModalOpen}:{setIsModalOpen:Dispatch<SetStateAction<boolean>>}) => {
+import {  motion } from "motion/react";
+import { Dispatch, SetStateAction } from "react";
 
 
+interface CreateRoomModalProps {
+  setIsModalOpen: Dispatch<SetStateAction<boolean>>;
+}
 
+export const CreateRoomModal = ({ setIsModalOpen }: CreateRoomModalProps) => {
   return (
-    <AnimatePresence>
-            <div
-      onClick={() =>{()=>{setIsModalOpen(false)
-        console.log("clicekd")
-       }}}
-      className="fixed inset-0 flex  justify-center bg-black/50 z-50"
-    >
-      <motion.div
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-        transition={{ type: "spring", stiffness: 200, bounce: 0.3, mass: 0.1, duration: 0.8 }}
-        onClick={(e) => e.stopPropagation()}
-        className="w-80 p-6 rounded-lg h-72 mt-[100px] bg-white shadow-lg font-primary tracking-tight"
+      <div
+        onClick={() => {
+          setIsModalOpen(false);
+          console.log("clicked");
+        }}
+        className="fixed inset-0 flex justify-center bg-black/70 z-50"
       >
-        <h1 className="font-semibold text-2xl mb-4">Filter</h1>
 
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ type: "spring", stiffness: 200, bounce: 0.3, mass: 0.1, duration: 0.8 }}
+          onClick={(e) => e.stopPropagation()}
+          className="w-80 p-6 rounded-lg h-72 mt-[100px] bg-black border border-gray-600/70  shadow-lg font-primary tracking-tight"
+        >
+        </motion.div>
 
-
-      </motion.div>
-    </div>
-    </AnimatePresence>
-
+      </div>
   );
 };
--- apps/excalidraw-frontend/components/DashboardClient.tsx
+++ apps/excalidraw-frontend/components/DashboardClient.tsx
@@ -4,13 +4,17 @@ import { useState } from "react";
 import DashboardMain from "./DashboardMain";
 import { DashboardNavbar } from "./DashboardNavbar";
 import { CreateRoomModal } from "./CreateRoomModal";
+import { AnimatePresence } from "motion/react";
 
 export default function DashboardClient() {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
         <div>
-            {isModalOpen && <CreateRoomModal />}
+            <AnimatePresence>
+            {isModalOpen && <CreateRoomModal setIsModalOpen={setIsModalOpen}/>}
+
+            </AnimatePresence>
             <DashboardNavbar />
             <DashboardMain isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
         </div>
